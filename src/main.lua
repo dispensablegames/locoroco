fe62e1eb7fe7f9f209fd72bb7d0f55632b89e291
Your Name <you@example.com>
2019-07-25 20:55:36 -0700

    add real locoroco level

--- src/main.lua
+++ src/main.lua
@@ -12,7 +12,7 @@ function love.load()
 
 	world = love.physics.newWorld(0, 9.81*16, true)
 
-	level = Level:init("levels/level1.svg")
+	level = Level:init("levels/world1level1.svg")
 	
 	locos = {}
 	love.graphics.setBackgroundColor(255, 255, 255)
