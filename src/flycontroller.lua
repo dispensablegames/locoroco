066ea2db00ad0687841ac505830edf3a3ab1651e
Your Name <you@example.com>
2019-08-06 18:13:27 -0700

    add image to flies

--- src/flycontroller.lua
+++ src/flycontroller.lua
@@ -1,3 +1,4 @@
+local Drawing = require("drawing")
 local Fly = require("fly")
 FlyController = {}
 
@@ -7,6 +8,9 @@ function FlyController:init(world)
 	finishedController.world_ = world
 	finishedController.fliesTotal_ = nil
 	finishedController.fliesCollected_ = 0
+	local flyDrawing = Drawing:init("src/assets/fly.svg")
+	local image = love.graphics.newImage(flyDrawing:toImageData())
+	finishedController.image_ = image
 	self.__index = self
 	setmetatable(finishedController, self)
 
@@ -16,7 +20,7 @@ end
 
 function FlyController:addFlies(pointsTable)
 	for i=1, #pointsTable/2 do
-		local fly = Fly:init(self.world_, pointsTable[2*i - 1], pointsTable[2*i], i)
+		local fly = Fly:init(self.world_, pointsTable[2*i - 1], pointsTable[2*i], i, self.image_)
 		table.insert(self.flies_, i, fly)
 	end
 	self.fliesTotal_ = #pointsTable/2
